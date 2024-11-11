928a7959e6b49211fc62d070aa08e41cc4ac3ea8
j <jgautama7@gmail.com>
2024-11-10 19:38:16 -0600

    update

--- TESTn2/zz099-qqzi1zz20629/support/boop.bat
+++ TESTn2/zz099-qqzi1zz20629/support/boop.bat
@@ -1,3 +1,3 @@
 @echo off
-start "" "C:\TESTn2\zz099-qqzi1zz20629\readme.txt"
+start "" "..\TESTn2\zz099-qqzi1zz20629\readme.txt"
 start /min python "..\TESTn2\zz08-chbin1zz20629\655-rezon889033\a\abin\pytest\wtz9pls10_21Dzzntz_99327.py"
